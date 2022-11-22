e102999ea867c561e3ccba17f113d3a12643472f
biruk_anley <birukanley2022@gmail.com>
2022-11-22 10:52:39 +0300

    update social media links

--- front_end_portfolio/src/components/SocialMedia.jsx
+++ front_end_portfolio/src/components/SocialMedia.jsx
@@ -1,23 +1,38 @@
 import React from 'react';
-import { BsTwitter, BsInstagram,BsLinkedin } from 'react-icons/bs';
+import { BsTwitter, BsInstagram,BsLinkedin,BsGithub } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
+
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsLinkedin/>
+      <a href="https://www.linkedin.com/in/biruk-anley/" target='_blank'>
+      <BsLinkedin />
+      </a>
     </div>
     
     <div>
-      <BsInstagram />
+      <a href="https://github.com/biruk-anley" target='_blank'>
+      <BsGithub />
+      </a>
     </div>
     <div>
+     
+      <a href="https://www.facebook.com/profile.php?id=100088309890637" target='_blank'>
       <FaFacebookF />
+      </a>
     </div>
+    
     <div>
+      <a href="https://twitter.com/AnleyBiruk" target='_blank'>
       <BsTwitter />
+      </a>
+      
     </div>
+    
+   
   </div>
+  
 );
 
 export default SocialMedia;
